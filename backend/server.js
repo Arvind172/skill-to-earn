cb52353f9e8a9f24c3a17d015974b9e63d832a98
Arvind172 <reacharvindnow@gmail.com>
2026-01-11 23:28:39 +0530

    io fix

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,15 @@ const bcrypt = require("bcryptjs");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://skill-to-earn-frontend.onrender.com",
+    ],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 const Freelancer = require("./models/Freelancer.model");
@@ -204,12 +212,15 @@ const { Server } = require("socket.io");
 
 const server = http.createServer(app);
 
-const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173",
-    methods: ["GET", "POST"],
-  },
-});
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://skill-to-earn-frontend.onrender.com",
+    ],
+    credentials: true,
+  })
+);
 
 app.post("/api/chats/start", auth, async (req, res) => {
   const { taskId, freelancerId } = req.body;
